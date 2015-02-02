3fce9adae75f8014527722b6259710ab45f5e5aa
Simon Gardner <gigner@gmail.com>
2015-02-02 20:20:49 +0000

    Lesson 1 - Sunshine app using static data with ListViews

--- app/src/androidTest/java/com/gigner/android/sunshine/ApplicationTest.java
+++ app/src/androidTest/java/com/gigner/android/sunshine/ApplicationTest.java
@@ -6,10 +6,10 @@ import android.test.ApplicationTestCase;
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
  */
-public class ApplicationTest extends ApplicationTestCase<Application>
+public class ApplicationTest extends ApplicationTestCase< Application >
 {
   public ApplicationTest()
   {
-    super(Application.class);
+    super( Application.class );
   }
 }
