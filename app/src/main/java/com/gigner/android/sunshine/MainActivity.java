c4bb0eb36b1a1f47830686caae00afc0c8a5f37b
Simon Gardner <gigner+github@gmail.com>
2015-02-09 23:06:59 +0000

    Lesson 2 - Working with the cloud (Needs review)

--- app/src/main/java/com/gigner/android/sunshine/MainActivity.java
+++ app/src/main/java/com/gigner/android/sunshine/MainActivity.java
@@ -1,19 +1,9 @@
 package com.gigner.android.sunshine;
 
 import android.support.v7.app.ActionBarActivity;
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 
 public class MainActivity extends ActionBarActivity
@@ -27,7 +17,7 @@ public class MainActivity extends ActionBarActivity
     if ( savedInstanceState == null )
     {
       getSupportFragmentManager().beginTransaction()
-              .add( R.id.container, new PlaceholderFragment() )
+              .add( R.id.container, new ForecastFragment() )
               .commit();
     }
   }
@@ -58,42 +48,4 @@ public class MainActivity extends ActionBarActivity
     return super.onOptionsItemSelected( item );
   }
 
-  /**
-   * A placeholder fragment containing a simple view.
-   */
-  public static class PlaceholderFragment extends Fragment
-  {
-
-    ArrayAdapter<String> forecastAdapter;
-
-    public PlaceholderFragment()
-    {
-    }
-
-    @Override
-    public View onCreateView( final LayoutInflater inflater, final ViewGroup container,
-                              final Bundle savedInstanceState )
-    {
-      final View rootView = inflater.inflate( R.layout.fragment_main, container, false );
-
-      final String forecastArray[] = {
-              "Today - Sunny - 88/63",
-              "Tomorrow - Foggy - 70/46",
-              "Wed - Foggy - 72/63",
-              "Thur - Cloudy - 62/51",
-              "Fri - Rainy - 72/46",
-              "Sat - Foggy - 76/68",
-              "Sun - Sunny - 88/68"
-      };
-      final List<String> weekForecast = new ArrayList<>( Arrays.asList(forecastArray));
-      forecastAdapter =
-              new ArrayAdapter<>( getActivity(),
-                      R.layout.list_item_forcast, R.id.list_item_forcast_textview, weekForecast );
-      final ListView listView = (ListView)rootView.findViewById( R.id.listview_forecast );
-      listView.setAdapter( forecastAdapter );
-
-
-      return rootView;
-    }
-  }
 }
