654d27a5a89682c23d8bbe3258a3276fac111315
Simon Gardner <gigner+github@gmail.com>
2015-03-09 23:28:46 +0000

    Lesson 3, 4a, 4b, 4c-2

--- app/src/main/java/com/gigner/android/sunshine/app/SettingsActivity.java
+++ app/src/main/java/com/gigner/android/sunshine/app/SettingsActivity.java
@@ -1,4 +1,4 @@
-package com.gigner.android.sunshine.activity;
+package com.gigner.android.sunshine.app;
 
 import android.os.Bundle;
 import android.preference.ListPreference;
@@ -6,24 +6,24 @@ import android.preference.Preference;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceManager;
 
+import com.gigner.android.sunshine.R;
+
 /**
  * A {@link PreferenceActivity} that presents a set of application settings.
- * <p/>
+ * <p>
  * See <a href="http://developer.android.com/design/patterns/settings.html">
  * Android Design: Settings</a> for design guidelines and the <a
  * href="http://developer.android.com/guide/topics/ui/settings.html">Settings
  * API Guide</a> for more information on developing a Settings UI.
  */
 public class SettingsActivity extends PreferenceActivity
-    implements Preference.OnPreferenceChangeListener
-{
+        implements Preference.OnPreferenceChangeListener {
 
   @Override
-  public void onCreate( Bundle savedInstanceState )
-  {
-    super.onCreate( savedInstanceState );
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
     // Add 'general' preferences, defined in the XML file
-    addPreferencesFromResource( R.xml.preferences );
+    addPreferencesFromResource( R.xml.preferences);
 
     // For all preferences, attach an OnPreferenceChangeListener so the UI summary can be
     // updated when the preference changes.
@@ -36,39 +36,35 @@ public class SettingsActivity extends PreferenceActivity
    * Also fires the listener once, to initialize the summary (so it shows up before the value
    * is changed.)
    */
-  private void bindPreferenceSummaryToValue( Preference preference )
-  {
+  private void bindPreferenceSummaryToValue(Preference preference) {
     // Set the listener to watch for value changes.
-    preference.setOnPreferenceChangeListener( this );
+    preference.setOnPreferenceChangeListener(this);
 
     // Trigger the listener immediately with the preference's
     // current value.
-    onPreferenceChange( preference,
-        PreferenceManager
-            .getDefaultSharedPreferences( preference.getContext() )
-            .getString( preference.getKey(), "" ) );
+    onPreferenceChange(preference,
+            PreferenceManager
+                    .getDefaultSharedPreferences( preference.getContext() )
+                    .getString( preference.getKey(), "" ));
   }
 
   @Override
-  public boolean onPreferenceChange( Preference preference, Object value )
-  {
+  public boolean onPreferenceChange(Preference preference, Object value) {
     String stringValue = value.toString();
 
-    if ( preference instanceof ListPreference )
-    {
+    if (preference instanceof ListPreference) {
       // For list preferences, look up the correct display value in
       // the preference's 'entries' list (since they have separate labels/values).
-      ListPreference listPreference = ( ListPreference ) preference;
-      int prefIndex = listPreference.findIndexOfValue( stringValue );
-      if ( prefIndex >= 0 )
-      {
-        preference.setSummary( listPreference.getEntries()[ prefIndex ] );
+      ListPreference listPreference = (ListPreference) preference;
+      int prefIndex = listPreference.findIndexOfValue(stringValue);
+      if (prefIndex >= 0) {
+        preference.setSummary(listPreference.getEntries()[prefIndex]);
       }
     }
     else
     {
       // For other preferences, set the summary to the value's simple string representation.
-      preference.setSummary( stringValue );
+      preference.setSummary(stringValue);
     }
     return true;
   }
